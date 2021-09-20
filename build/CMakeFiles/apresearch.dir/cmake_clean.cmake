56b87cb0120de17f4ec0b619a44bc4956433409c
jonahgluck <61380701+jonahgluck@users.noreply.github.com>
2021-09-20 17:58:08 -0400

    first commit

---
+++ build/CMakeFiles/apresearch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/apresearch.dir/src/main.cpp.o"
+  "apresearch"
+  "apresearch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/apresearch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <torch/torch.h>
+int main(){
+    torch::Tensor x = torch::randn({3, 3}, torch::kCUDA);
+    std::cout << x << std::endl;
+}
